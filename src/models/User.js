e81e911ba30c55d01edc7d96b2ce06fcb8f8ecbd
hussien <hussiens236@gmail.com>
2024-06-22 18:20:21 +0300

    update user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,27 @@
-
 const mongoose = require('mongoose');
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: { 
+    type: String,
+    required: [ true , "please tell us your name!" ] 
+  },
+  email: { 
+    type: String, 
+    required: [true, "please provide your email" ] ,
+    unique: true,
+    lowercase: true ,
+    validate: [ validator.isEmail , "please provide a valid email "]
+  },
+  password: { 
+    type: String, 
+    required: [true , "please provide a password"] ,
+    minlength: 8 
+  },
+  passwordConfirm: {
+    type: String , 
+    required: [ true , "please confirm your password"]
+  }
 });
 
 const User = mongoose.model('User', userSchema);
